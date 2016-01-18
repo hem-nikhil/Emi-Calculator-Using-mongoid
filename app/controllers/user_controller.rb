00d9cb6d6d6b0bdfa9b4a7796b6db61d5391ddc3
hem-nikhil <nikhil@hem.com>
2016-01-18 09:05:27 +0530

    lazy loading n+1 problem solved

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,12 +2,7 @@ class UserController < ApplicationController
 
 	# display all users along with their emi enquires
 	def show
-		@user_info = {}
-		users = User.all
-		users.each do |user|
-			@user_info[user.id] = {}
-			@user_info[user.id][:email] = user.email
-			@user_info[user.id][:emi_enquiries] = user.emi_enquiries
-		end
+		@user_info = User.get_user_info
+		
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User
 	has_many :emi_enquiries
 	validates :email, presence: true, email: true
 	validates_uniqueness_of :email 
+
+	class << self
+		def get_user_info
+			user_info = {}
+			users = includes(:emi_enquiries).all
+			users.each do |user|
+				user_info[user.id] = {}
+				user_info[user.id][:email] = user.email
+				user_info[user.id][:emi_enquiries] = user.emi_enquiries
+			end
+			user_info
+		end
+	end
 end
